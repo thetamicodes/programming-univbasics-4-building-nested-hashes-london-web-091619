91b56a5dcd039abeeee10770216708046eab19f6
Tami Reichold <reichold.tamara@gmail.com>
2019-09-10 17:46:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,15 @@
 def base_hash
-  hash = {key}
+  hash = {
+    key
+  }
   return hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  hash = {
+    key
+  }
+  return hash
 end
 
 def monopoly_with_third_tier
