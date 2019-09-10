dd285f0a135c59ca0e70ce53cf67f6b2c519bebf
Tami Reichold <reichold.tamara@gmail.com>
2019-09-10 18:08:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,8 @@ end
 
 def monopoly_with_third_tier
    monopoly = {
-    :railroads => {
+    :railroads=> 
+    {
       :pieces => 4, 
       :rent_in_dollars => {
         one_piece_owned: 25, 
